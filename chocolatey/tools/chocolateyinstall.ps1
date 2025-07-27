ef674b2348209e0d236b9baa384fd19d21ed23f9
ragebreaker <125530737+mlm-games@users.noreply.github.com>
2025-07-27 09:37:21 +0530

    Update chocolateyinstall.ps1

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -2,14 +2,14 @@ $ErrorActionPreference = 'Stop'
 
 $packageName = 'imagot'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/mlm-games/imagot/releases/download/0.2.4/imagot.exe'
+$url = 'https://github.com/mlm-games/imagot/releases/download/0.2.7/imagot.exe'
 
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'Imagot*'
-  checksum      = '0000000000000000000000000000000000000000000000000000000000000000'
+  checksum      = '5149abfa095683421c7061326e60fe65734fbd2c7f373d2141ba1fa6355e1d04'
   checksumType  = 'sha256'
   silentArgs    = "/S"
   validExitCodes= @(0)
